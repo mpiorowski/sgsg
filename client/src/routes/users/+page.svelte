38520ef3bc5fff89469e41f57b7636a3961198a0
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-01-02 00:13:13 +0100

    update

--- client/src/routes/users/+page.svelte
+++ client/src/routes/users/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import { enhance } from "$app/forms";
-    import { Button } from "@mpiorowski/svelte-init";
     import type { PageServerData } from "./$types";
 
     export let data: PageServerData;
@@ -14,10 +12,6 @@
                 <pre>
                 {JSON.stringify(user, null, 2)}
                 </pre>
-                <form action="?/delete" use:enhance method="post" id={user.id}>
-                    <input type="hidden" name="user" value={JSON.stringify(user)} />
-                    <Button form={user.id}>Delete</Button>
-                </form>
             </li>
         {/each}
     </ul>
