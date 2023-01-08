43127b11b447d53696dcb0998d6e6bc9ea7d47b9
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-01-08 13:48:42 +0100

    update

--- client/src/routes/files/+page.svelte
+++ client/src/routes/files/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { enhance } from "$app/forms";
     import type { PageData } from "./$types";
 
     export let data: PageData;
@@ -10,9 +11,10 @@
     <p>{file.name}</p>
     <p>{file.id}</p>
     <p>{file.targetId}</p>
+    <a href={file.url}>Download</a>
 {/each}
 
-<form action="?/file" method="post" enctype="multipart/form-data">
+<form action="?/file" method="post" id="file" use:enhance>
     <input type="file" name="file" />
-    <input type="submit" value="Upload" />
+    <input type="submit" value="Upload" form="file" />
 </form>
