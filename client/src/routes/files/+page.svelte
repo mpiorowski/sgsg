0620386fd1de372d2948800b10c107fd6d01f6fb
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-01-08 15:44:55 +0100

    update

--- client/src/routes/files/+page.svelte
+++ client/src/routes/files/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { enhance } from "$app/forms";
+    import { Button } from "@mpiorowski/svelte-init";
     import type { PageData } from "./$types";
 
     export let data: PageData;
@@ -8,13 +9,17 @@
 <h1>Files</h1>
 
 {#each data.files as file}
-    <p>{file.name}</p>
-    <p>{file.id}</p>
-    <p>{file.targetId}</p>
-    <a href={file.url}>Download</a>
+    <pre>{JSON.stringify(file, null, 2)}</pre>
+    <Button
+        on:click={() => {
+            window.open(file.url, "_blank");
+        }}
+    >
+        Download
+    </Button>
 {/each}
 
 <form action="?/createFile" method="post" id="file" use:enhance>
     <input type="file" name="file" />
-    <button type="submit" form="file">Upload</button>
+    <Button form="file">Upload</Button>
 </form>
