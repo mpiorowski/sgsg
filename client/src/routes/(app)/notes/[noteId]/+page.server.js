70bcd0abf74f4fcd115ba812f5c737ac1fb145ce
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-10-23 22:55:00 +0200

    update

--- client/src/routes/(app)/notes/[noteId]/+page.server.js
+++ client/src/routes/(app)/notes/[noteId]/+page.server.js
@@ -3,10 +3,10 @@ import { getFormValue } from "$lib/helpers";
 import { createMetadata } from "$lib/server/metadata";
 import { server } from "$lib/server/grpc";
 import { grpcSafe } from "$lib/safe";
+import { schema } from "./note";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals, params }) {
-
     const id = params.noteId;
     if (!id) {
         throw error(409, "Missing note id");
@@ -32,12 +32,28 @@ export const actions = {
     update: async ({ locals, request }) => {
         const form = await request.formData();
 
-        /** @type {import("$lib/proto/proto/Note").Note} */
+        const validation = schema.safeParse({
+            id: form.get("id"),
+            title: form.get("title"),
+            content: form.get("content"),
+        });
+
+        if (!validation.success) {
+            const fieldErrors = validation.error.flatten().fieldErrors;
+            const fields = Object.entries(fieldErrors).map(([key, value]) => ({
+                field: key,
+                tag: value.join(", "),
+            }));
+            return fail(400, { fields: fields });
+        }
+
+        /** @type {import("./type").Note} */
         const data = {
-            id: getFormValue(form, "id"),
-            title: getFormValue(form, "title"),
-            content: getFormValue(form, "content"),
+            id: validation.data.id,
+            title: validation.data.title,
+            content: validation.data.content,
         };
+
         const metadata = createMetadata(locals.token);
         /** @type {import("$lib/safe").Safe<import("$lib/proto/proto/Note").Note__Output>} */
         const req = await new Promise((r) => {
