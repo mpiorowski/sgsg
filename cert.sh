3cd71afaf68e1aeb3ce688f650ec5171037df59a
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-11-15 10:34:49 +0100

    update

--- cert.sh
+++ cert.sh
@@ -1,6 +1,6 @@
 apt-get install certbot python3-certbot-nginx
 
-sudo certbot certonly --nginx -d www.sgsg.bearbyte.org -d sgsg.bearbyte.org
+sudo certbot certonly --nginx -d $1
 
 sudo certbot renew --dry-run
 
