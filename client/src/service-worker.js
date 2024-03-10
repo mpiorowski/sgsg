bb54d95c13de0d6d333b46016613172cac4fbf80
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-03-10 21:46:31 +0100

    update

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,63 +1,87 @@
-// @ts-nocheck
 /// <reference types="@sveltejs/kit" />
-import { build, files, version } from '$service-worker';
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+const sw = /** @type {ServiceWorkerGlobalScope} */ (
+    /** @type {unknown} */ (self)
+);
+import { build, files, version } from "$service-worker";
 
 // Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
 
 const ASSETS = [
-	...build, // the app itself
-	...files  // everything in `static`
+    ...build, // the app itself
+    ...files, // everything in `static`
 ];
 
-self.addEventListener('install', (event) => {
-	// Create a new cache and add all files to it
-	async function addFilesToCache() {
-		const cache = await caches.open(CACHE);
-		await cache.addAll(ASSETS);
-	}
+sw.addEventListener("install", (event) => {
+    // Create a new cache and add all files to it
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
+        await cache.addAll(ASSETS);
+    }
 
-	event.waitUntil(addFilesToCache());
+    event.waitUntil(addFilesToCache());
 });
 
-self.addEventListener('activate', (event) => {
-	// Remove previous cached data from disk
-	async function deleteOldCaches() {
-		for (const key of await caches.keys()) {
-			if (key !== CACHE) await caches.delete(key);
-		}
-	}
+sw.addEventListener("activate", (event) => {
+    // Remove previous cached data from disk
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
+        }
+    }
 
-	event.waitUntil(deleteOldCaches());
+    event.waitUntil(deleteOldCaches());
 });
 
-self.addEventListener('fetch', (event) => {
-	// ignore POST requests etc
-	if (event.request.method !== 'GET') return;
+sw.addEventListener("fetch", (event) => {
+    // ignore POST requests etc
+    if (event.request.method !== "GET") return;
 
-	async function respond() {
-		const url = new URL(event.request.url);
-		const cache = await caches.open(CACHE);
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
 
-		// `build`/`files` can always be served from the cache
-		if (ASSETS.includes(url.pathname)) {
-			return cache.match(url.pathname);
-		}
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            const response = await cache.match(url.pathname);
 
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
-		try {
-			const response = await fetch(event.request);
+            if (response) {
+                return response;
+            }
+        }
 
-			if (response.status === 200) {
-				cache.put(event.request, response.clone());
-			}
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
 
-			return response;
-		} catch {
-			return cache.match(event.request);
-		}
-	}
+            // if we're offline, fetch can return a value that is not a Response
+            // instead of throwing - and we can't pass this non-Response to respondWith
+            if (!(response instanceof Response)) {
+                throw new Error("invalid response from fetch");
+            }
 
-	event.respondWith(respond());
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch (err) {
+            const response = await cache.match(event.request);
+
+            if (response) {
+                return response;
+            }
+
+            // if there's no cache, then just error out
+            // as there is nothing we can do to respond to this request
+            throw err;
+        }
+    }
+
+    event.respondWith(respond());
 });
