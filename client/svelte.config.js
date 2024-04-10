38acebcebb60ad6115fb89760ad5e34b78897904
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-04-10 11:17:47 +0200

    update

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from "@sveltejs/adapter-node";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
